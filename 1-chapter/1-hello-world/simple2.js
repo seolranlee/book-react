c973aaffe6a360863be0d46908a736d9c0bc8eaf
seolranlee <leesugar90@gmail.com>
2020-10-21 18:47:47 +0900

    1.2 리액트 개발 환경 직접 구축하기-Hello World 페이지 만들기

---
+++ 1-chapter/1-hello-world/simple2.js
@@ -0,0 +1,25 @@
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            liked : false
+        }
+    }
+    
+    render() {
+        const text = this.state.liked ? '좋아요 취소' : '좋아요'
+        return React.createElement(
+            'button',
+            {
+                onClick: () => this.setState({
+                    liked: true
+                })
+            },
+            text,
+        )
+    }
+}
+
+ReactDOM.render(React.createElement(LikeButton), document.querySelector('#react-root-1'))
+ReactDOM.render(React.createElement(LikeButton), document.querySelector('#react-root-2'))
+ReactDOM.render(React.createElement(LikeButton), document.querySelector('#react-root-3'))
