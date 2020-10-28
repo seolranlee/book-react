2c7a19d16f2c56e643a335f4f09945019d6c6a09
seolranlee <leesugar90@gmail.com>
2020-10-28 17:15:09 +0900

    1.3 create-react-app으로 시작하기 - core-js 패키지를 이용한 폴리필

--- cra-test/src/App.js
+++ cra-test/src/App.js
@@ -3,11 +3,14 @@ import smallImage from './small.png';
 import bigImage from './big.jpeg';
 
 class App extends Component {
+  
   render() {
+    const value = '123'.padStart(5, '0')
     return (
       <div className="App">
         <img src={bigImage} />
         <img src={smallImage} />
+        <p>{value}</p>
       </div>
     );
   }
