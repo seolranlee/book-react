7f235e6cb69d4cfb53f0e1573681f4bb4bab9ab4
seolranlee <leesugar90@gmail.com>
2020-10-28 17:56:25 +0900

    1.3 create-react-app으로 시작하기 - 환경 변수 사용하기

--- cra-test/src/App.js
+++ cra-test/src/App.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import smallImage from './small.png';
 import bigImage from './big.jpeg';
 import TodoList from './TodoList';
+import './test.css'
 
+console.log(`NODE_ENV = ${process.env.NODE_ENV}`)
+console.log(`REACT_APP_DATA_API = ${process.env.REACT_APP_DATA_API}`)
+console.log(`REACT_APP_LOGIN_API = ${process.env.REACT_APP_LOGIN_API}`)
 class App extends Component {
   
   render() {
